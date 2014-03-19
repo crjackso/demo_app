ee198de1519e1ddcc738cbe540d4871dfe3a4879
Chris Jackson <crjackso@gmail.com>
2014-03-19 18:33:01 -0500

    removing globally scoped javascript

--- app/assets/javascripts/participants.js
+++ app/assets/javascripts/participants.js
@@ -1,28 +1,43 @@
 
-$(document).ready(function(){
-        var influencer = getInfluencer();
-        console.log(influencer);
-    $('#signup_prompt').click(function(event){
-        var microsite = window.location.hostname;
-        console.log(influencer);
-        console.log(microsite);
-        event.preventDefault();
-        return false;
-    });
+cf.participantMgr = function() {
+    var self = this;
+    var signup_form = $('#new_participant');
+    var submit_button = $('#signup_prompt');
+
+    function getJsonFromUrl() {
+        var query = location.search.substr(1);
+        var data = query.split("&");
+        var result = {};
+        for(var i=0; i<data.length; i++) {
+            var item = data[i].split("=");
+            result[item[0]] = item[1];
+        }
+        return result;
+    }
+
+    function getInfluencer() {
+        var queryStrings = getJsonFromUrl();
+        return queryStrings && queryStrings.influencer;
+    }
+
+    self.init = function(){
+        submit_button.click(function(event){
+            event.preventDefault();
+            var influencer = getInfluencer();
+            var microsite = window.location.hostname;
+            console.log(influencer);
+            console.log(microsite);
+
+            signup_form.submit();
+            return false;
+        });
+    }
+
+    return self;
+};
+
+$(document).ready(function() {
+    var mgr = cf.participantMgr();
+    mgr.init();
 });
 
-function getJsonFromUrl() {
-    var query = location.search.substr(1);
-    var data = query.split("&");
-    var result = {};
-    for(var i=0; i<data.length; i++) {
-        var item = data[i].split("=");
-        result[item[0]] = item[1];
-    }
-    return result;
-}
-
-function getInfluencer() {
-    var queryStrings = getJsonFromUrl();
-    return queryStrings && queryStrings.influencer;
-}
