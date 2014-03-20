f5f6c8ec29898bcaafca666273f4b2e6591877dc
msvivianso <thoughtworker@VivianSo.local>
2014-03-20 11:54:47 -0500

    [Alberto/Vivian] added microsite model tests

--- spec/models/microsite_spec.rb
+++ spec/models/microsite_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Microsite do
+
+  it 'should be valid' do
+    microsite = FactoryGirl.build(:microsite)
+    microsite.should be_valid
+  end
+
+  context 'when name is not present' do
+    it 'should not be valid' do
+      microsite = FactoryGirl.build(:microsite)
+      microsite.name = ''
+      microsite.should_not be_valid
+    end
+  end
 end
