f93f49b888fec800ff02aea6e712c9a5d33c1a2e
msvivianso <thoughtworker@VivianSo.local>
2014-03-20 16:21:54 -0500

    [Alberto/Vivian] refactored urlhelper spec

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,6 +1,7 @@
 module UrlHelper
   def generate_unique_url(microsite, influencer_id)
-    return microsite if influencer_id == 0
+    return microsite if influencer_id.nil?
+
     hashids = Hashids.new("this is my salt")
     hash = hashids.encrypt(influencer_id)
     return "#{microsite}/?influencer=#{hash}"
--- spec/helpers/url_helper_spec.rb
+++ spec/helpers/url_helper_spec.rb
@@ -4,14 +4,15 @@ describe UrlHelper do
   describe "unique url generation" do
     it "should return base microsite if influencer is not specified" do
       microsite = 'nowisourmoment.org'
-      unique_url = generate_unique_url(microsite, 0)
+      unique_url = generate_unique_url(microsite, nil)
       unique_url.should == microsite
     end
 
     it "should return unique url given a valid microsite and influencer" do
       microsite = 'nowisourmoment.org'
       unique_url = generate_unique_url(microsite, 1)
-      unique_url.should == 'nowisourmoment.org/?influencer=NV'
+      unique_url.should_not  == 'nowisourmoment.org/?influencer=1'
+      unique_url.should match(/\?influencer=[A-Z]+/)
     end
   end
 end
